5fb023e3997acd0795ea1f6b4ce2d2fea2bf4bc0
Heunsig <heun3344@gmail.com>
2025-02-09 20:27:33 +0900

    fix: improve layout spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="flex flex-col min-h-screen dark:bg-gray-900">
-    <header class="flex items-center justify-between p-3 border-b dark:border-gray-800">
-      <NuxtLink to="/">
-        <h1 class="text-lg font-bold dark:text-gray-50">SaaS Explorer</h1>
-      </NuxtLink>
+    <header 
+      class="px-4 py-3 flex items-center justify-between border-b dark:border-gray-800"
+    >
+      <div class="container mx-auto xl:max-w-7xl">
+        <NuxtLink to="/projects">
+          <h1 class="text-lg font-bold dark:text-gray-50">SnapDock</h1>
+        </NuxtLink>
+      </div>
     </header>
     <main 
-      class="grid flex-1"
+      class="grid flex-1 px-4"
     >
-      <div class="p-4 container mx-auto xl:max-w-7xl">
+      <div class="py-8 md:py-10 container mx-auto xl:max-w-7xl">
         <slot />
       </div>
     </main>
-    <footer class="p-4 border-t dark:border-gray-800">
-      SaaS Explorer
+    <footer class="p-4 py-3 border-t dark:border-gray-800">
+      <div class="container mx-auto xl:max-w-7xl">
+        <NuxtLink to="/projects">
+          <h1 class="text-lg font-bold dark:text-gray-50">SnapDock</h1>
+        </NuxtLink>
+      </div>
     </footer>
   </div>
 </template>
