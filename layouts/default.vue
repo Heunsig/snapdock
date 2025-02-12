1d86a8a9e088224a289c20f63a98cb7ef97f1c7c
Heunsig <heun3344@gmail.com>
2025-02-13 00:35:33 +0900

    feat: Add logo image to header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
+
 <template>
   <div class="flex flex-col min-h-screen dark:bg-gray-900">
     <header 
       class="px-4 py-3 flex items-center justify-between border-b dark:border-gray-800"
     >
       <div class="container mx-auto xl:max-w-7xl">
-        <NuxtLink to="/projects">
-          <h1 class="text-lg font-bold dark:text-gray-50">SnapDock</h1>
+        <NuxtLink to="/projects" class="inline-flex items-center gap-2">
+          <NuxtImg src="/assets/images/logo.png" alt="SnapDock" width="48"/>
+          <h1 class="text-xl font-bold dark:text-gray-50">SnapDock</h1>
         </NuxtLink>
       </div>
     </header>
@@ -16,11 +18,19 @@
         <slot />
       </div>
     </main>
-    <footer class="p-4 py-3 border-t dark:border-gray-800">
+    <footer class="p-4 py-20 border-t dark:border-gray-800">
       <div class="container mx-auto xl:max-w-7xl">
-        <NuxtLink to="/projects">
-          <h1 class="text-lg font-bold dark:text-gray-50">SnapDock</h1>
-        </NuxtLink>
+        <div class="grid md:grid-cols-2 gap-y-6 gap-x-12 md:gap-x-25">
+          <div class="flex flex-col gap-6 max-w-sm">
+            <NuxtLink to="/projects" class="inline-flex items-center gap-2">
+              <NuxtImg src="/assets/images/logo.png" alt="SnapDock" width="48"/>
+              <h1 class="text-lg font-bold dark:text-gray-50">SnapDock</h1>
+            </NuxtLink>
+            <p class="max-w-lg text-sm md:text-base text-gray-500 dark:text-gray-400 italic pl-1.5">
+              SnapDock is a collection of tools and guides to help you get started with Docker.
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   </div>
