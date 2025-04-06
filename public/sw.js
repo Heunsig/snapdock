860b9123c4755647e6fee532115c04bbf0e60266
Heunsig <heun3344@gmail.com>
2025-04-06 09:01:52 +0900

    fix: [Vue Router warn]: No match found for location with path "/sw.js" warn

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+// This empty file is added to prevent the error:
+// [Vue Router warn]: No match found for location with path "/sw.js"
+// Vue tries to resolve this route, so we include it in the public folder.
